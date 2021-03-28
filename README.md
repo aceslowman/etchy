514c6feebd2b07b155bc9e79eb9ad1c78f26ebe0
Glitch (hello-express) <none>
2021-03-28 03:13:54 +0000

    🦄🖇 Checkpoint ./server/APIServer.js:24872531/3150 ./README.md:24872531/1169

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Public Sphere
 
+## Basic Node Schema
+
+```
+{
+  uuid: nanoid(),
+  nickname: string,
+  address: hash of global coordinate,
+  broadcasting: boolean
+}
+```
+
+Each node belongs to a _sphere_, which is the main social group.
+Anyone can install a sphere on their own hosted server and allocate a set
+amount of space. 
---
+++ server/APIServer.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const fs = require("fs");
+const app = express(); 
+const { nanoid } = require('nanoid');
+
+function checkHttps(req, res, next){
+  // protocol check, if http, redirect to https
+  
+  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+    return next()
+  } else {
+    res.redirect('https://' + req.hostname + req.url);
+  }
+}
+
+app.all("*", checkHttps);
+
+// A test route to make sure the server is up.
+app.get("/api/data", (request, response) => {
+  console.log("❇️ Received GET request to /api/data");
+  response.json({"message": "hello world"});
+});
+
+app.get("/api/shaders", (request, response) => {
+  // response.json(shader_collection);
+});
+
+// Express port-switching logic
+let port;
+console.log("❇️ NODE_ENV is", process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  port = process.env.PORT || 3000;
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.use(express.static(path.join(__dirname, "../public")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+} else {
+  port = 3001;
+}
+
+// Start the listener!
+const listener = app.listen(port, () => {
+  console.log("❇️ Express server is running on port", listener.address().port);
+});
