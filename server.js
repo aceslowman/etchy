c7840a46a7c76247f8c30653f216bb714a29c36c
Glitch (hello-express) <none>
2021-03-28 18:38:44 +0000

    🎐🎭 Checkpoint ./server.js:24872531/9503

--- server.js
+++ server.js
@@ -1,34 +1,76 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
+const { nanoid } = require('nanoid');
+const WebSocket = require('ws');
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static("public")); 
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+function checkHttps(req, res, next){
+  // protocol check, if http, redirect to https
+  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+    return next()
+  } else {
+    res.redirect('https://' + req.hostname + req.url);
+  }
+} 
+
+app.all("*", checkHttps);
+
+// A test route to make sure the server is up.
+app.get("/api/data", (request, response) => {
+  console.log("❇️ Received GET request to /api/data");
+  response.json({"message": "hello world"});
 });
 
+app.get("/api/shaders", (request, response) => {
+  // response.json(shader_collection);
+});
+
+app.get("/api/sphere/info", (request, response) => {
+  response.json({
+    name: 'test sphere',
+    size: 0,
+    
+  });
+});
+
+app.get("/api/members", (request, response) => {
+  response.json([{
+     
+  }]);
+});
+
+app.get("/api/member", (request, response) => {
+  response.json({
+    // uuid: 
+    // nickname:
+    // address:   // inherit from a 'node' class
+  });
+});
+
+app.get("/api/garden", (request, response) => {
+  response.json({
+    // uuid:
+    // nickname:
+    // address:   // inherit from a 'node' class
+    // size:  
+  });
+});
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+// const listener = app.listen(process.env.PORT, () => {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
