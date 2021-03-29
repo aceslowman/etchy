7ae115747e1bf8ba458895b570f2c54f3b0bc6e7
Glitch (hello-express) <none>
2021-03-29 01:57:55 +0000

    🌝⛹ Checkpoint ./server.js:24872531/8009

--- server.js
+++ server.js
@@ -1,76 +1,19 @@
-const express = require("express");
-const app = express();
-
-const { nanoid } = require('nanoid');
+const fs = require('fs');
+const https = require('https');
 const WebSocket = require('ws');
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public")); 
 
-const wss = new WebSocket.Server({ server: app });
+const server = https.createServer({
+  cert: fs.readFileSync('/path/to/cert.pem'),
+  key: fs.readFileSync('/path/to/key.pem')
+});
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    }); 
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
   });
+
+  ws.send('something');
 });
 
-function checkHttps(req, res, next){
-   // protocol check, if http, redirect to https
-  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
-    return next()
-  } else {
-    res.redirect('https://' + req.hostname + req.url);
-  }
-} 
-
-app.all("*", checkHttps);
-
-// A test route to make sure the server is up.
-app.get("/api/data", (request, response) => {
-  console.log("❇️ Received GET request to /api/data");
-  response.json({"message": "hello world"});
-});
-
-app.get("/api/shaders", (request, response) => {
-  // response.json(shader_collection);
-});
-
-app.get("/api/sphere/info", (request, response) => {
-  response.json({
-    name: 'test sphere',
-    size: 0,
-    
-  });
-});
-
-app.get("/api/members", (request, response) => {
-  response.json([{
-     
-  }]);
-});
-
-app.get("/api/member", (request, response) => {
-  response.json({
-    // uuid: 
-    // nickname:
-    // address:   // inherit from a 'node' class
-  });
-});
-
-app.get("/api/garden", (request, response) => {
-  response.json({
-    // uuid:
-    // nickname:
-    // address:   // inherit from a 'node' class
-    // size:  
-  });
-});
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+server.listen(process.env.PORT);
