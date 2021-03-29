5bb49a8d030b41e4124319970248665db77253c2
Glitch (hello-express) <none>
2021-03-29 02:52:04 +0000

    💆🎓 Checkpoint ./server.js:24872531/123

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
-var server = require("http").createServer(),
-  WebSocketServer = require("ws").Server,
-  server = require("http").createServer(),
-  wss = new WebSocketServer({ server: server }),
-  express = require("express"),
-  app = express();
+// var server = require("http").createServer(),
+//   WebSocketServer = require("ws").Server,
+//   server = require("http").createServer(),
+//   wss = new WebSocketServer({ server: server }),
+//   express = require("express"),
+//   app = express();
 
-// Client stuff
-app.use(express.static("public"));
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-// Server stuff
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(message) {
-    ws.send(message);
-  });
-});
-
-// Listen
-server.on("request", app);
-server.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + server.address().port);
-});
-
-// const express = require('express');
-// const app = express();
-
-// var expressWs = require('express-ws')(app);
- 
-// app.use(function (req, res, next) {
-//   console.log('middleware');
-//   req.testing = 'testing';
-//   return next();
+// // Client stuff
+// app.use(express.static("public"));
+// app.get("/", function(request, response) {
+//   response.sendFile(__dirname + "/views/index.html");
 // });
- 
-// app.get('/', function(req, res, next){
-//   console.log('get route', req.testing);
-//   res.end();
-// });
- 
-// app.ws('/', function(ws, req) {
-//   ws.on('message', function(msg) {
-//     console.log(msg);
+
+// // Server stuff
+// wss.on("connection", function connection(ws) {
+//   ws.on("message", function incoming(message) {
+//     ws.send(message);
 //   });
-//   console.log('socket', req.testing);
 // });
+
+// // Listen
+// server.on("request", app);
+// server.listen(process.env.PORT, function() {
+//   console.log("Your app is listening on port " + server.address().port);
+// });
+
+const express = require('express');
+const app = express();
+
+var expressWs = require('express-ws')(app);
  
-// app.listen(process.env.PORT);
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+ 
+app.get('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+ 
+app.ws('/', function(ws, req) {
+  ws.on('message', function(msg) {
+    console.log(msg);
+  });
+  console.log('socket', req.testing);
+});
+ 
+app.listen(process.env.PORT);
