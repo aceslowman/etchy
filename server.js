6c33f117b8945ea94f03fde1ca8d114ada416887
Glitch (hello-express) <none>
2021-03-29 16:15:36 +0000

    👐⚾️ Checkpoint ./server.js:24872531/2626

--- server.js
+++ server.js
@@ -1,50 +1,52 @@
-// var server = require("http").createServer(),
-//   WebSocketServer = require("ws").Server,
-//   server = require("http").createServer(),
-//   wss = new WebSocketServer({ server: server }),
-//   express = require("express"),
-//   app = express();
-
-// // Client stuff
-// app.use(express.static("public"));
-// app.get("/", function(request, response) {
-//   response.sendFile(__dirname + "/views/index.html");
-// });
-
-// // Server stuff
-// wss.on("connection", function connection(ws) {
-//   ws.on("message", function incoming(message) {
-//     ws.send(message);
-//   });
-// });
-
-// // Listen
-// server.on("request", app);
-// server.listen(process.env.PORT, function() {
-//   console.log("Your app is listening on port " + server.address().port);
-// });
-
+const http = require('http');
 const express = require('express');
+const ws = require('ws');
+
 const app = express();
 
-var expressWs = require('express-ws')(app);
- 
-app.use(function (req, res, next) {
-  console.log('middleware');
-  req.testing = 'testing';
-  return next();
-});
- 
-app.get('/', function(req, res, next){
-  console.log('get route', req.testing);
-  res.end();
-});
- 
-app.ws('/', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log(msg);
+// we need to create our own http server so express and ws can share it.
+const server = http.createServer(app);
+// pass the created server to ws
+const wss = new ws.Server({ server });
+
+// we're using an ES2015 Set to keep track of every client that's connected
+let sockets = new Set();
+
+// based on https://www.npmjs.com/package/ws#simple-server
+wss.on('connection', function connection(ws) {
+  sockets.add(ws);
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
   });
-  console.log('socket', req.testing);
+  
+  ws.on('close', function () {
+    sockets.delete(ws);
+    // tell everyone a client left
+    update();
+  });
+  
+  // tell everyone a client joined
+  update();
 });
+
+function update() {
+  // send an updated client count to every open socket.
+  sockets.forEach(ws => ws.send(JSON.stringify({
+    type: 'count',
+    count: sockets.size
+  })));
+}
  
-app.listen(process.env.PORT);
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests!
+const listener = server.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
