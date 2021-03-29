504d93b2121ca35dd544dfe9965ac5845ac4030a
Glitch (hello-express) <none>
2021-03-29 02:40:39 +0000

    🍇🏂 Checkpoint ./package.json:24872531/1376 ./server.js:24872531/704

--- server.js
+++ server.js
@@ -23,3 +23,28 @@ server.on("request", app);
 server.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + server.address().port);
 });
+
+// const express = require('express');
+// const app = express();
+
+// var expressWs = require('express-ws')(app);
+ 
+// app.use(function (req, res, next) {
+//   console.log('middleware');
+//   req.testing = 'testing';
+//   return next();
+// });
+ 
+// app.get('/', function(req, res, next){
+//   console.log('get route', req.testing);
+//   res.end();
+// });
+ 
+// app.ws('/', function(ws, req) {
+//   ws.on('message', function(msg) {
+//     console.log(msg);
+//   });
+//   console.log('socket', req.testing);
+// });
+ 
+// app.listen(process.env.PORT);
