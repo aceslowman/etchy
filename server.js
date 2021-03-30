e310d670417338859b1287234ff8d253aabc3828
Glitch (hello-express) <none>
2021-03-30 02:53:58 +0000

    🚏🍭 Checkpoint ./server.js:24872531/2518 ./package.json:24872531/1434 ./views/index.html:24872531/2491

--- server.js
+++ server.js
@@ -1,52 +1,43 @@
 const http = require('http');
-const express = require('express');
-const ws = require('ws');
+const express = require("express");
+const path = require('path');
+const fs = require('fs').promises
+const WebSocket = require('ws');
+const markdownit = require('markdown-it')()
 
 const app = express();
-
-// we need to create our own http server so express and ws can share it.
 const server = http.createServer(app);
-// pass the created server to ws
-const wss = new ws.Server({ server });
+const wss = new WebSocket.Server({ server: server });
 
-// we're using an ES2015 Set to keep track of every client that's connected
-let sockets = new Set();
+// Renders the README as HTML
+app.get("/", async (request, response) => {
+  const handle = await fs.open(path.join(__dirname, 'README.md'), 'r')
+  try {
+    const contents = await handle.readFile('utf-8')
+    const html = markdownit.render(contents)
+    response.send(html);
+  } finally {
+    handle.close()
+  }
+});
 
-// based on https://www.npmjs.com/package/ws#simple-server
+app.get("/client", async (request, response) => {
+  const handle = await fs.open(path.join(__dirname, 'client.html'), 'r')
+  try {
+    const contents = await handle.readFile('utf-8')
+    response.send(contents);
+  } finally {
+    handle.close()
+  }
+});
+
 wss.on('connection', function connection(ws) {
-  sockets.add(ws);
-
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message); 
+    console.log('received: %s', message);
+    ws.send('reply: ' + message)
   });
-  
-  ws.on('close', function () {
-    sockets.delete(ws);
-    // tell everyone a client left
-    update();
-  });
-  
-  // tell everyone a client joined
-  update();
+
+  ws.send('something');
 });
 
-function update() {
-  // send an updated client count to every open socket.
-  sockets.forEach(ws => ws.send(JSON.stringify({
-    type: 'count',
-    count: sockets.size
-  })));
-}
- 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests!
-const listener = server.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+server.listen(process.env.PORT)
