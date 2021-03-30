6f199df07d411ed57811af8bc5576d4bfdb1793c
Glitch (hello-express) <none>
2021-03-30 04:49:08 +0000

    💋🌊 Checkpoint ./client/scripts/App.js:24872531/12747 ./client/index.html:24872531/103

--- client/scripts/App.js
+++ client/scripts/App.js
@@ -1,4 +1,67 @@
-/* global React, ReactDOM */
+/* global THREE, React, ReactDOM */
+
+const Sphere = () => {
+  const container = React.useRef();
+  let 
+
+  const init = () => {
+    // let container = document.getElementById("glcontainer");
+    let bounds = container.current.getBoundingClientRect();
+    console.log("bounds", bounds);
+
+    const geometry = new THREE.PlaneGeometry(512, 512);
+    const material = new THREE.MeshBasicMaterial({
+      //CHANGED to MeshBasicMaterial
+      // map: spraycanImg,
+      map: circleImg,
+      magFilter: THREE.NearestFilter
+    });
+    material.map.needsUpdate = true;
+
+    symbol = new THREE.Mesh(geometry, material);
+    scene.add(symbol);
+
+    renderer = new THREE.WebGLRenderer();
+    // renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(bounds.width, bounds.height);
+    renderer.setClearColor(0x000000, 0);
+    renderer.autoClear = false;
+
+    container.appendChild(renderer.domElement);
+
+    // stats = new Stats();
+    // container.appendChild(stats.dom);
+  };
+
+  React.useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <div
+      style={{
+        flexGrow: 2,
+        position: "relative",
+        overflow: "overlay"
+      }}
+    >
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+        ref={container}
+      >
+        something
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
     <ThemeContext.Provider
@@ -13,28 +76,9 @@ const App = () => {
         <Settings>
           <InputPanel title="basic">
             <Button onClick={() => {}}>register</Button>
-          </InputPanel>  
+          </InputPanel>
         </Settings>
-        <div
-          style={{
-            flexGrow: 2,
-            position: "relative",
-            overflow: "overlay"
-          }}
-        >
-          <div
-            style={{
-              width: "100%",
-              height: "100%",
-              position: "absolute",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-          >
-            something
-          </div>
-        </div>
+        <Sphere />
       </AppWrapper>
     </ThemeContext.Provider>
   );
