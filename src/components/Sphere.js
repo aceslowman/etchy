8d81c7c542684e74162b9f80473d3ff2086b4500
Glitch (hello-express) <none>
2021-04-06 16:52:24 +0000

    🍏🏄 Checkpoint ./src/components/Sphere.js:24872531/2500

--- src/components/Sphere.js
+++ src/components/Sphere.js
@@ -1,12 +1,20 @@
 import * as THREE from "three";
 import OrbitControls from "three/examples/jsm/controls/OrbitControls.js";
 
+class Marker {
+  constructor(_label, _position, _metadata) {
+    this.label = _label;
+    this.position = _position;
+    this.metadata = _metadata;
+  }
+}
+
 class Sphere {
   constructor() {
     this.solid_geo = new THREE.SphereGeometry(150, 32, 32);
     this.solid_mat = new THREE.MeshBasicMaterial();
     // this.mesh = new THREE.Mesh(this.solid_geo, this.solid_mat);
-    
+
     this.smaller_geo = new THREE.SphereGeometry(148, 32, 32);
     this.mesh = new THREE.Mesh(this.smaller_geo, this.solid_mat);
 
@@ -14,13 +22,16 @@ class Sphere {
     this.wire_mat = new THREE.LineBasicMaterial({ color: "#5F6FFF" });
     this.wireframe = new THREE.LineSegments(this.wire_geo, this.wire_mat);
   }
-  
-  update() {
-//     this.mesh.rotation.x += 0.005;
-//     this.mesh.rotation.y += 0.01;
 
-//     this.wireframe.rotation.x += 0.005;
-//     this.wireframe.rotation.y += 0.01;      
+  update() {
+    //     this.mesh.rotation.x += 0.005;
+    //     this.mesh.rotation.y += 0.01;
+    //     this.wireframe.rotation.x += 0.005;
+    //     this.wireframe.rotation.y += 0.01;
+  }
+
+  addMarker() {
+    this.markers.push(new Marker("test"));
   }
 }
 
