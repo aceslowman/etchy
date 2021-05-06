f7669f806775f1e1b78c388ec937fb26963c4add
Glitch (hello-express) <none>
2021-05-06 17:58:11 +0000

    🍥🌊 Checkpoint ./server/server.js:24872531/9880

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
-const http = require('http');
-const express = require('express');
-const ws = require('ws');
+// https://stackoverflow.com/questions/6860853/generate-random-string-for-div-id
+function guidGenerator() {
+  var S4 = function() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (
+    S4() +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    S4() +
+    S4()
+  );
+}
+
+const http = require("http");
+const express = require("express");
+const ws = require("ws");
 
 const app = express();
 
@@ -11,38 +32,40 @@ const wss = new ws.Server({ server });
 // let sockets = new Set();
 let connections = new Map();
 
-wss.on('connection', function connection(ws) {
+wss.on("connection", function connection(ws) {
   // console.log('ws', ws)
   // sockets.add({
   //   socket: ws
   // });
+  let id = guidGenerator();
+  connections.set(id, { uuid: id, socket: ws });
 
-  ws.on('message', function incoming(m) {
-    let message = JSON.parse(m); 
-    
-    
-    switch(message.type) {
-      case 'PITCH':
-        console.log('pitch', message);
-        
+  ws.on("message", function incoming(m) {
+    let message = JSON.parse(m);
+
+    switch (message.type) {
+      case "PITCH":
+        console.log("pitch", message);
+        let con = connections.get(id);
+        connections.set(message.uuid, { ...con, pitch: message.pitch });
         break;
-      case 'REGISTER':
-        console.log('register', message);
+      case "REGISTER":
+        console.log("register", message);
         break;
       default:
-        console.log('message received without TYPE')
+        console.log("message received without TYPE");
         break;
     }
-    
+
     // console.log(sockets)
   });
-  
-  ws.on('close', function () {
+
+  ws.on("close", function() {
     // connections.delete(ws);
     // tell everyone a client left
     updateCount();
   });
-  
+
   // tell everyone a client joined
   updateCount();
 });
@@ -53,17 +76,25 @@ function updateCount() {
   //   type: 'COUNT',
   //   count: sockets.size
   // })));
+  connections.forEach(con => {
+    con.ws.send(
+      JSON.stringify({
+        type: "COUNT",
+        count: connections.size
+      })
+    );
+  });
 }
- 
+
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 // listen for requests!
 const listener = server.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
