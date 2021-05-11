9dc0a4c53295c67c0b506ebdce20980509ff70f8
Glitch (hello-express) <none>
2021-05-11 01:48:43 +0000

    🏯🐮 Checkpoint ./server/server.js:24872531/616 ./public/index.html:24872531/94

--- server/server.js
+++ server/server.js
@@ -28,15 +28,9 @@ const app = express();
 const server = http.createServer(app);
 const wss = new ws.Server({ server });
 
-// we're using an ES2015 Set to keep track of every client that's connected
-// let sockets = new Set();
 let connections = new Map();
 
 wss.on("connection", function connection(ws) {
-  // console.log('ws', ws)
-  // sockets.add({
-  //   socket: ws
-  // });
   let id = guidGenerator();
   connections.set(id, { uuid: id, socket: ws });
 
@@ -56,26 +50,18 @@ wss.on("connection", function connection(ws) {
         console.log("message received without TYPE");
         break;
     }
-
-    // console.log(sockets)
   });
 
   ws.on("close", function() { 
+    console.log('deleting connection')
     connections.delete(id); 
-    // tell everyone a client left
     updateCount();
   });
 
-  // tell everyone a client joined
   updateCount();
 });
 
 function updateCount() {
-  // send an updated client count to every open socket.
-  // sockets.forEach(ws => ws.send(JSON.stringify({
-  //   type: 'COUNT',
-  //   count: sockets.size
-  // })));
   connections.forEach(con => {
     con.socket.send(
       JSON.stringify({
@@ -86,15 +72,12 @@ function updateCount() {
   });
 }
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// listen for requests!
 const listener = server.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
