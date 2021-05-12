292ea5e4455730c924450d9310edbc5b2571d2f4
Glitch (hello-express) <none>
2021-05-12 23:17:56 +0000

    🐪⛵️ Checkpoint ./server.js:24872531/320 ./public/index.html:24872531/1264

--- server.js
+++ server.js
@@ -176,6 +176,20 @@ function updateCount() {
   });
 }
 
+function checkHttps(req, res, next){
+  // protocol check, if http, redirect to https
+  
+  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+    console.log("https, yo")
+    return next()
+  } else {
+    console.log("just http")
+    res.redirect('https://' + req.hostname + req.url);
+  }
+}
+
+app.all('*', checkHttps)
+
 app.use(express.static("public"));
 
 app.get("/", function(request, response) {
