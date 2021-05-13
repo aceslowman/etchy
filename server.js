0826d491024a11b040ceb762787fa45e5ecc8898
Glitch (hello-express) <none>
2021-05-13 00:06:13 +0000

    👨‍❤️‍💋‍👨🚨 Checkpoint ./public/index.html:24872531/23964 ./server.js:24872531/112

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ wss.on("connection", ws => {
         connections.set(id, { ...con, pitch: message.pitch });
         break;
       case "OFFER":
-        // console.log("OFFER", message.sdp);
+        console.log("OFFER", message.sdp);
         break;
       case "ANSWER":
         console.log("ANSWER");
@@ -177,13 +177,9 @@ function updateCount() {
 }
 
 function checkHttps(req, res, next){
-  // protocol check, if http, redirect to https
-  
   if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
-    console.log("https, yo")
     return next()
   } else {
-    console.log("just http")
     res.redirect('https://' + req.hostname + req.url);
   }
 }
