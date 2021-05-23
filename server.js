f9797859da244096af48acb6648ec8f96884b1ce
Glitch (hello-express) <none>
2021-05-23 01:08:56 +0000

    🍮👚 Checkpoint ./oldServerFASTRTC.js:24872531/3054 ./server.js:24872531/2656

--- server.js
+++ server.js
@@ -7,6 +7,92 @@ const app = express();
 const server = http.createServer(app);
 const wss = new ws.Server({ server });
 
+class CandidateSignal {
+  type: 'candidate' = 'candidate'
+  constructor (public candidate: object | null) {}
+}
+
+class OfferSignal {
+  type: 'offer' = 'offer'
+  constructor (public sdp: string) {}
+}
+
+class ConnectionChunk {
+  // signals = []// Array<OfferSignal | CandidateSignal>
+
+  constructor (id, sdp) {
+    this.signals = [new OfferSignal(sdp)]
+  }
+}
+
+export default class WebSocketContext {
+  userId?: string
+  createdAt: number
+  connectedPeers: {[connectionId: string]: WebSocketId} = {}
+  pushQueue: ConnectionChunk[] = []
+  pullQueue: string[] = []
+  subs: number[] = []
+  roomId: string
+  constructor (roomId: string) {
+    this.roomId = roomId
+    this.createdAt = Date.now()
+  }
+}
+
+const closeWRTC = (ws: UWebSocket) => {
+  if (!ws.context) return
+  const {userId, roomId, subs} = ws.context
+  const redis = getPubSub()
+  subs.forEach((subId) => redis.unsubscribe(subId))
+  subs.length = 0
+  if (userId) {
+    redis.publish(`signal/room/${roomId}`, JSON.stringify({type: 'leaveSwarm', userId})).catch()
+  }
+  delete ws.context
+}
+
+const handleAccept = (ws: UWebSocket, payload: SwarmAccept) => {
+  ws.context.connectedPeers[payload.id] = payload.userId
+  sendSignal(ws, payload)
+}
+
+const handleAnswer = (ws: UWebSocket, payload: AnswerPayload) => {
+  const to = ws.context.connectedPeers[payload.id]
+  getPubSub()
+    .publish(
+      `signal/user/${to}`,
+      JSON.stringify({
+        type: 'pubToClient',
+        payload
+      })
+    )
+    .catch()
+}
+
+const handleCandidate = (ws: UWebSocket, payload: CandidatePayloadToServer) => {
+  const {candidate, id} = payload
+  const {context} = ws
+  // if (!candidate) return
+  const to = context.connectedPeers[id]
+  if (to) {
+    // the receiver is known
+    getPubSub()
+      .publish(
+        `signal/user/${to}`,
+        JSON.stringify({
+          type: 'pubToClient',
+          payload: {type: 'candidate', id, candidate}
+        })
+      )
+      .catch()
+    return
+  }
+  const existingChunk = context.pushQueue.find((connectionChunk) => connectionChunk.id === id)
+  if (existingChunk) {
+    existingChunk.signals.push(new CandidateSignal(candidate))
+  }
+}
+
 let connections = new Map();
 
 wss.on("connection", ws => {
