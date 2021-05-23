9d2c40ca93018c4b438a0c4bff17acd6753a86a9
Glitch (hello-express) <none>
2021-05-23 01:54:06 +0000

    💈🐾 Checkpoint ./server.js:24872531/3481 ./src/oldmain.js:24872531/3976 ./src/index.js:24872531/5165 ./package.json:24872531/4015

--- server.js
+++ server.js
@@ -4,119 +4,18 @@ const ws = require("ws");
 
 const swarm = require('webrtc-swarm');
 const signalhub = require("signalhub");
-const hub = signalhub('etchy', ['https://etchy.glitch.me']);
+const hub = signalhub('etchy', ['https://etchy.glitch.me:3000']);
+
+hub.subscribe('etchy')
+  .on('data', function (message) {
+    console.log('new message received', message)
+  })
+
+hub.broadcast('etchy', {hello: 'world'})
 
 const app = express();
 
-var sw = swarm(hub, { 
-  // wrtc: require('wrtc') // don't need this if used in the browser
-})
-
-sw.on('peer', function (peer, id) {
-  console.log('connected to a new peer:', id)
-  console.log('total peers:', sw.peers.length)
-})
-
-sw.on('disconnect', function (peer, id) {
-  console.log('disconnected from a peer:', id)
-  console.log('total peers:', sw.peers.length)
-})
-
 const server = http.createServer(app);
-// const wss = new ws.Server({ server });
-
-// let connections = new Map();
-
-// wss.on("connection", ws => {
-//   let id;
-
-//   ws.on("message", m => {
-//     let message = JSON.parse(m);
-//     // console.log(message);
-//     console.log('connections', connections)
-//     switch (message.type) {
-//       case "offer":
-//         console.log("OFFER", message.id);
-//         // send offer to all *other* peers        
-//         if(connections[message.id])
-//           connections[message.id].socket.send(JSON.stringify(message));
-//         // connections.forEach(con => {          
-//         //   // if (con.sid !== message.sid) {            
-//         //     con.socket.send(
-//         //       JSON.stringify(message)
-//         //     );
-//         //   // }
-//         // });
-//         break;
-//       case "answer":
-//         console.log("ANSWER", message.id);
-//         // send answer to all *other* peers\
-//         // console.log('connections', connections)
-//         if(connections[message.id])
-//           connections[message.id].socket.send(JSON.stringify(message));
-//         // connections.forEach(con => {
-//         //   // if (con.sid !== message.sid) {
-//         //     con.socket.send(
-//         //       JSON.stringify(message)
-//         //     );
-//         //   // }
-//         // });        
-//         break;
-//       case "candidate":
-//         console.log("CANDIDATE", message.id);
-//         // send ans5wer to all *other* peers
-//         // console.log('connections', connections)
-//         if(connections[message.id])
-//           connections[message.id].socket.send(JSON.stringify(message));        
-//         // connections.forEach(con => {
-//         //   // if (con.sid !== message.sid) {1
-//         //     con.socket.send(
-//         //       JSON.stringify(message)
-//         //     );
-//         //   // }
-//         // });
-//         break;
-//       case "init":
-//         console.log("initializing user", message.userId);
-//         id = message.userId;
-//         connections.set(id, {
-//           id: id,
-//           socket: ws
-//         });
-//         ws.send(JSON.stringify({type:'registered',id:id}));
-//         updateCount();
-//         break;
-//       default:
-//         connections.forEach(con => {
-//           // if (con.userId !== message.userId) {
-//             con.socket.send(JSON.stringify(message));
-//           // }
-//         });
-//         break;
-//     }
-    
-//   });
-
-//   ws.on("close", () => {
-//     console.log("deleting connection", id);
-//     connections.delete(id);
-//     updateCount();
-//   });
-
-//   updateCount();
-// });
-
-// function updateCount() {
-//   connections.forEach(con => {
-//     con.socket.send(
-//       JSON.stringify({
-//         type: "count",
-//         count: connections.size,
-//         peers: Array.from(connections.values()).map(e => e.id)
-//       })
-//     );
-//   });
-// }
 
 function checkHttps(req, res, next) {
   if (req.get("X-Forwarded-Proto").indexOf("https") != -1) {
---
+++ src/oldmain.js
@@ -0,0 +1,133 @@
+import FriendlyWebSocket from "./FriendlyWebSocket";
+import FastRTCSwarm from "@mattkrick/fast-rtc-swarm";
+
+// const cam = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+const socket = new FriendlyWebSocket({ path: "/" }).socket;
+// const socket = new WebSocket('wss://etchy.glitch.me/');
+
+socket.addEventListener("open", () => {
+  navigator.mediaDevices
+    .getUserMedia({ audio: false, video: true })
+    .then(stream => {
+      const swarm = new FastRTCSwarm();
+
+      console.log("got user media", stream);
+      localStream = stream;
+      started = true;
+
+      swarm.on("signal", signal => {
+        socket.send(JSON.stringify(signal));
+      });
+
+      socket.addEventListener("message", event => {
+        // console.log("got a message", event.data);
+        const payload = JSON.parse(event.data);
+
+        switch (payload.type) {
+          case "registered":
+            document.querySelector(".yourId").innerText = `your id: ${payload.id}`;
+            user_id = payload.id;
+            console.log("registered", payload.id);
+            for (const track of localStream.getTracks()) {
+              console.log("adding track to peer connection", track);
+              swarm.addStreams({ track });
+            }
+            break;
+          case "count":
+            console.log('message', payload)
+            document.querySelector(
+              ".count"
+            ).innerText = `currently online: ${payload.count}`;
+            document.querySelector(
+              ".peers"
+            ).innerText = `currently online: [${payload.peers}]`;
+            break;
+          default:
+            swarm.dispatch(payload);
+        }
+      });
+
+      // when the connection is open, say hi to your new peer
+      swarm.on("dataOpen", peer => {
+        console.log("data channel open!");
+        peer.send("hi");
+      });
+
+      // when your peer says hi, log it
+      swarm.on("data", (data, peer) => {
+        console.log("data received", data, peer);
+      });
+
+      // fired when a peer creates or updates an audio/video track.
+      swarm.on("stream", (stream, peer) => {
+        console.log("Add streaming element", stream);
+        //         const el = document.createElement("video");
+        //         el.autoplay = true;
+        //         el.controls = true; // TEMP
+
+        //         if (event.streams && event.streams[0]) {
+        //           el.srcObject = event.streams[0];
+        //         } else {
+        //           let inboundStream = new MediaStream(event.track);
+        //           el.srcObject = inboundStream;
+        //         }
+
+        //         el.play();
+
+        //         document.querySelector("#remoteStreams").appendChild(el);
+      });
+
+      swarm.on("error", (error, peer) => {
+        console.error(error);
+      });
+    })
+    .catch(err => {
+      console.log("Error capturing stream.", err);
+    });
+});
+
+let user_id;
+let peers = {};
+let pendingCandidates = {};
+let localStream;
+
+let started = false;
+
+let canvas = document.getElementById("mainCanvas");
+let ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const send = data => {
+  socket.send(JSON.stringify(data));
+};
+
+// const init = () => {
+//   if (started) return;
+
+//   //   document.querySelector(".center").innerText = "";
+
+//   //   navigator.mediaDevices
+//   //     .getUserMedia({ audio: false, video: true })
+//   //     .then(stream => {
+//   //       console.log("got user media", stream);
+//   //       localStream = stream;
+//   //       started = true;
+//   //     })
+//   //     .catch(err => {
+//   //       console.log("Error capturing stream.", err);
+//   //     });
+// };
+
+const drawOnCanvas = () => {};
+
+const onWindowResize = e => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  drawOnCanvas();
+};
+
+drawOnCanvas();
+
+// document.addEventListener("click", init, false);
+window.addEventListener("resize", onWindowResize, false);
