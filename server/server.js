b216544bf48a6e38507c951caa3ec62c991869a8
Glitch (hello-express) <none>
2021-05-23 04:31:31 +0000

    🐚❄️ Checkpoint ./server/oldServer.js:24872531/3641 ./server/server.js:24872531/6695 ./src/index.js:24872531/23063

--- server/server.js
+++ server/server.js
@@ -1,16 +1,34 @@
+// https://stackoverflow.com/questions/6860853/generate-random-string-for-div-id
+function guidGenerator() {
+  var S4 = function() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return (
+    S4() +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    "-" +
+    S4() +
+    S4() +
+    S4()
+  );
+}
+
+// https://stackoverflow.com/questions/6454198/check-if-a-value-is-within-a-range-of-numbers
+function between(x, min, max) {
+  return x >= min && x <= max;
+}
+
 const http = require("http");
 const express = require("express");
 const ws = require("ws");
 
 const app = express();
-
-const {
-  handleInit,
-  WebSocketContext,
-  handlers,
-  handleSignal
-} = require("./Signaling");
-// console.log(handlers);
 
 const server = http.createServer(app);
 const wss = new ws.Server({ server });
@@ -19,43 +37,34 @@ let connections = new Map();
 
 wss.on("connection", ws => {
   let id;
-  let wsctx = {socket: ws, context: new WebSocketContext(0)}
 
   ws.on("message", m => {
     let message = JSON.parse(m);
-    console.log('incoming type:',message.type);
 
-    // handleSignal(ws.socket, message);
-
-    // switch (message.type) {
-    //   case "init":
-    //     handleInit(wsctx, message);
-    //     break;
-    //   default:
-    //     console.log("message received without TYPE");
-    //     break;
-    // }
-    
     switch (message.type) {
+      case "register":
+        console.log("registering user", message.sid);
+        id = message.sid;
+        connections.set(id, {
+          sid: id,
+          socket: ws,
+        });
+        updateCount();
+        break;
       case "offer":
-        console.log("OFFER", message.id);
-        // send offer to all *other* peers        
-        // if(connections[message.id])
-        //   connections[message.id].socket.send(JSON.stringify(message));
-        connections.forEach(con => {
-          // if (con.sid !== message.sid) {
+        console.log("OFFER", message.sid);
+        // send offer to all *other* peers
+        connections.forEach(con => {          
+          // if (con.sid !== message.sid) {            
             con.socket.send(
               JSON.stringify(message)
             );
           // }
-        });  
+        });
         break;
       case "answer":
-        console.log("ANSWER", message.id);
-        // send answer to all *other* peers\
-        // console.log('connections', connections)
-        // if(connections[message.id])
-        //   connections[message.id].socket.send(JSON.stringify(message));
+        console.log("ANSWER", message.sid);
+        // send answer to all *other* peers
         connections.forEach(con => {
           // if (con.sid !== message.sid) {
             con.socket.send(
@@ -65,11 +74,8 @@ wss.on("connection", ws => {
         });        
         break;
       case "candidate":
-        console.log("CANDIDATE", message.id);
-        // send ans5wer to all *other* peers
-        // console.log('connections', connections)
-        // if(connections[message.id])
-        //   connections[message.id].socket.send(JSON.stringify(message));        
+        console.log("CANDIDATE", message.sid);
+        // send answer to all *other* peers
         connections.forEach(con => {
           // if (con.sid !== message.sid) {1
             con.socket.send(
@@ -78,24 +84,11 @@ wss.on("connection", ws => {
           // }
         });
         break;
-      case "init":
-        console.log("initializing user", message.userId);
-        id = message.userId;
-        connections.set(id, {
-          id: id,
-          socket: ws
-        });
-        ws.send(JSON.stringify({type:'registered',id:id}));
-        updateCount();
-        break;
       default:
-        connections.forEach(con => {
-          // if (con.userId !== message.userId) {
-            con.socket.send(JSON.stringify(message));
-          // }
-        });
+        console.log("message received without TYPE");
         break;
     }
+
   });
 
   ws.on("close", () => {
@@ -113,7 +106,7 @@ function updateCount() {
       JSON.stringify({
         type: "count",
         count: connections.size,
-        peers: Array.from(connections.values()).map(e => e.id)
+        peers: Array.from(connections.values()).map(e => e.sid)
       })
     );
   });
--- src/index.js
+++ src/index.js
@@ -1,99 +1,15 @@
 import FriendlyWebSocket from "./FriendlyWebSocket";
-import FastRTCSwarm from "@mattkrick/fast-rtc-swarm";
+// https://stackoverflow.com/questions/6860853/generate-random-string-for-div-id
+function guidGenerator() {
+  var S4 = function() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  };
+  return S4() + S4();
+}
 
-// const cam = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
-const socket = new FriendlyWebSocket({ path: "/" }).socket;
-// const socket = new WebSocket('wss://etchy.glitch.me/');
+let user_id = guidGenerator();
 
-socket.addEventListener("open", () => {
-  navigator.mediaDevices
-    .getUserMedia({ audio: false, video: true })
-    .then(stream => {
-      const swarm = new FastRTCSwarm();
-
-      console.log("got user media", stream);
-      localStream = stream;
-      started = true;
-
-      swarm.on("signal", (signal, peer) => {
-        console.log('sending signal', signal)
-        console.log('peer', peer)
-        socket.send(JSON.stringify(signal));
-      });
-
-      socket.addEventListener("message", event => {        
-        const payload = JSON.parse(event.data);
-        console.log("got a message", payload);
-
-        switch (payload.type) {
-          case "registered":
-            document.querySelector(".yourId").innerText = `your id: ${payload.id}`;
-            user_id = payload.id;
-            console.log("registered", payload.id);
-            for (const track of localStream.getTracks()) {
-              console.log("adding track to peer connection", track);
-              swarm.addStreams({ track });
-            }
-            break;
-          case "count":
-            console.log('message', payload)
-            document.querySelector(
-              ".count"
-            ).innerText = `currently online: ${payload.count}`;
-            document.querySelector(
-              ".peers"
-            ).innerText = `currently online: [${payload.peers}]`;
-            break;
-          default:
-            swarm.dispatch(payload);
-        }
-      });
-
-      // when the connection is open, say hi to your new peer
-      swarm.on("dataOpen", peer => {
-        console.log("data channel open!");
-        peer.send("hi");
-      });
-
-      // when your peer says hi, log it
-      swarm.on("data", (data, peer) => {
-        console.log("data received", data, peer);
-      });
-
-      // fired when a peer creates or updates an audio/video track.
-      swarm.on("stream", (stream, peer) => {
-        console.log("Add streaming element", stream);
-        //         const el = document.createElement("video");
-        //         el.autoplay = true;
-        //         el.controls = true; // TEMP
-
-        //         if (event.streams && event.streams[0]) {
-        //           el.srcObject = event.streams[0];
-        //         } else {
-        //           let inboundStream = new MediaStream(event.track);
-        //           el.srcObject = inboundStream;
-        //         }
-
-        //         el.play();
-
-        //         document.querySelector("#remoteStreams").appendChild(el);
-      });
-
-      swarm.on("error", (error, peer) => {
-        console.error(error);
-      });
-    
-      swarm.on('connection', (stream, peer) => {
-        console.log('connection change', peer)
-      })
-    })
-    .catch(err => {
-      console.log("Error capturing stream.", err);
-    });
-});
-
-let user_id;
-let peers = {};
+let peer;
 let pendingCandidates = {};
 let localStream;
 
@@ -104,26 +20,179 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const send = data => {
-  socket.send(JSON.stringify(data));
+let conConfig = {
+  iceServers: [{ url: "stun:stun.1.google.com:19302" }],
+  offerToReceiveAudio: false,
+  offerToReceiveVideo: true
 };
 
-// const init = () => {
-//   if (started) return;
+// ------------------------------------------------------------
+// setting up websocket signaling server
+const websocket = new FriendlyWebSocket({ path: "/" });
 
-//   //   document.querySelector(".center").innerText = "";
+const createPeerConnection = () => {
+  const pc = new RTCPeerConnection(conConfig);
+  pc.onicecandidate = onIceCandidate;
+  pc.ontrack = handleOnTrack;
 
-//   //   navigator.mediaDevices
-//   //     .getUserMedia({ audio: false, video: true })
-//   //     .then(stream => {
-//   //       console.log("got user media", stream);
-//   //       localStream = stream;
-//   //       started = true;
-//   //     })
-//   //     .catch(err => {
-//   //       console.log("Error capturing stream.", err);
-//   //     });
-// };
+  console.log("PeerConnection created");
+
+  for (const track of localStream.getTracks()) {
+    console.log("adding track to peer connection", track);
+    pc.addTrack(track, localStream);
+  }
+
+  return pc;
+};
+
+const sendOffer = sid => {
+  console.log("Send offer to " + sid);
+  peer
+    .createOffer()
+    .then(sdp => setAndSendLocalDescription(sid, sdp))
+    .catch(error => {
+      console.error("Send offer failed: ", error);
+    });
+};
+
+const sendAnswer = sid => {
+  console.log("Send answer to " + sid);
+  peer
+    .createAnswer()
+    .then(sdp => setAndSendLocalDescription(sid, sdp))
+    .catch(error => {
+      console.error("Send answer failed: ", error);
+    });
+};
+
+const setAndSendLocalDescription = (sid, sessionDescription) => {
+  console.log("sessionDescription", sessionDescription);
+  peer
+    .setLocalDescription(sessionDescription)
+    .then(() => {
+      send({ sid, type: sessionDescription.type, sdp: sessionDescription });
+      console.log("Local description set", sessionDescription);
+    })
+    .catch(error => {
+      console.error("issue with setting local description: ", error);
+    });
+};
+
+const onIceCandidate = event => {
+  if (event.candidate) {
+    console.log("ICE candidate", event);
+    send({
+      sid: user_id,
+      type: "candidate",
+      candidate: event.candidate
+    });
+  } else {
+    console.log("All ICE candidates have been sent");
+  }
+};
+
+const handleOnTrack = event => {
+  console.log("Add streaming element", event);
+  const newRemoteStreamElem = document.createElement("video");
+  newRemoteStreamElem.autoplay = true;
+  newRemoteStreamElem.controls = true; // TEMP
+
+  if (event.streams && event.streams[0]) {
+    newRemoteStreamElem.srcObject = event.streams[0];
+  } else {
+    let inboundStream = new MediaStream(event.track);
+    newRemoteStreamElem.srcObject = inboundStream;
+  }
+
+  newRemoteStreamElem.play();
+
+  document.querySelector("#remoteStreams").appendChild(newRemoteStreamElem);
+};
+
+const addPendingCandidates = sid => {
+  if (sid in pendingCandidates) {
+    pendingCandidates[sid].forEach(candidate => {
+      peer.addIceCandidate(candidate);
+    });
+  }
+};
+
+// REGISTER when connection opens
+websocket.on("open", data => {
+  document.querySelector(".yourId").innerText = `your id: ${user_id}`;
+  send({ type: "register", sid: user_id });
+  peer = createPeerConnection();
+});
+
+// when signaling server sends a message
+websocket.on("message", data => {
+  data = JSON.parse(event.data);
+  const sid = data.sid;
+  delete data.sid;
+
+  switch (data.type) {
+    case "count":
+      document.querySelector(
+        ".count"
+      ).innerText = `currently online: ${data.count}`;
+      document.querySelector(
+        ".peers"
+      ).innerText = `currently online: [${data.peers}]`;
+      break;
+    case "offer":
+      console.log("receiving offer from " + sid, data);
+      peer = createPeerConnection();
+      peer.setRemoteDescription(data.sdp).then(() => {
+        sendAnswer(sid);
+        addPendingCandidates(sid);
+      });
+      break;
+    case "answer":
+      console.log("receiving answer from " + sid, data);
+      // if (sid !== user_id) peers[sid].setRemoteDescription(data.sdp);
+      peer.setRemoteDescription(data.sdp);
+      break;
+    case "candidate":
+      // if (sid in peers) {
+      peer.addIceCandidate(data.ice);
+      // } else {
+      //   if (!(sid in pendingCandidates)) {
+      //     pendingCandidates[sid] = [];
+      //   }
+      //   pendingCandidates[sid].push(data.candidate);
+      // }
+      break;
+    default:
+      break;
+  }
+});
+
+const send = data => {
+  websocket.send(JSON.stringify(data));
+};
+
+const init = () => {
+  if (started) return;
+
+  document.querySelector(".center").innerText = "";
+
+  navigator.mediaDevices
+    .getUserMedia({ audio: false, video: true })
+    .then(stream => {
+      console.log("got user media", stream);
+      localStream = stream;
+
+      // initial connection
+      peer = createPeerConnection();
+
+      sendOffer(user_id);
+
+      started = true;
+    })
+    .catch(err => {
+      console.log("Error capturing stream.", err);
+    });
+};
 
 const drawOnCanvas = () => {};
 
@@ -135,5 +204,5 @@ const onWindowResize = e => {
 
 drawOnCanvas();
 
-// document.addEventListener("click", init, false);
+document.addEventListener("click", init, false);
 window.addEventListener("resize", onWindowResize, false);
