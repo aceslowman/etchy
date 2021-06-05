cf724315b9604d4af335778b952a0c57abe5914e
Glitch (hello-express) <none>
2021-06-05 16:56:10 +0000

    🍠🍝 Checkpoint ./package.json:24872531/1494 ./server/server.js:24872531/430

--- server/server.js
+++ server/server.js
@@ -28,6 +28,16 @@ const http = require("http");
 const express = require("express");
 const ws = require("ws");
 
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.tokens.create().then(token => console.log(token.username));
+
+
 const app = express();
 
 const server = http.createServer(app);
