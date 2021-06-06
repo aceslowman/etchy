5bb3672f15fc569c4082226413530752641ccd3b
Glitch (hello-express) <none>
2021-06-06 20:09:55 +0000

    💐🏞 Checkpoint ./src/shaders.js:24872531/8078

--- src/shaders.js
+++ src/shaders.js
@@ -38,9 +38,18 @@ const screenFrag = `
     vec2 uv0 = vTextureCoord;
     vec2 uv1 = vTextureCoord;
     
-    uv0.x /= texdim0.y / texdim0.x;
-    uv1.x /= texdim1.y / texdim1.x;
-
+    if(texdim0.x > texdim0.y) { // if wide
+      // uv0.x *= texdim0.x / texdim0.y;
+    } else {
+      uv0.y *= texdim0.y / texdim0.x;
+    }
+    
+    if(texdim1.x > texdim1.y) { // if wide
+      // uv1.x *= texdim1.x / texdim1.y;
+    } else {
+      uv1.y *= texdim1.y / texdim1.x;
+    }
+ 
     vec3 v0 = texture2D(tex0, uv0).rgb;
     vec3 v1 = texture2D(tex1, uv1).rgb;
     
@@ -76,9 +85,18 @@ const multiplyFrag = `
     vec2 uv0 = vTextureCoord;
     vec2 uv1 = vTextureCoord;
    
-    uv0.x /= texdim0.y / texdim0.x;
-    uv1.x /= texdim1.y / texdim1.x;
-
+    if(texdim0.x > texdim0.y) { // if wide
+      // uv0.x *= texdim0.x / texdim0.y;
+    } else {
+      uv0.y *= texdim0.y / texdim0.x;
+    }
+    
+    if(texdim1.x > texdim1.y) { // if wide
+      // uv1.x *= texdim1.x / texdim1.y;
+    } else {
+      uv1.y *= texdim1.y / texdim1.x;
+    }
+ 
     vec3 v0 = texture2D(tex0, uv0).rgb;
     vec3 v1 = texture2D(tex1, uv1).rgb;
     //gl_FragColor = vec4(vec3(texdim1),1.0);
